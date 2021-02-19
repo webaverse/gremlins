771957dcf8bf98e9fbc20fd5855564706d87bac2
Avaer Kazmer <a@modules.io>
2021-02-19 13:32:03 -0500

    Initialize quaternion

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ import {renderer, scene, app, runtime, npc} from 'app';
       -10 + Math.random() * 20,
       -10 + Math.random() * 20
     ));
+    const d = new THREE.Vector3(Math.random(), Math.random(), Math.random()).multiplyScalar(Math.PI*2);
+    const euler = new THREE.Euler(0, d.y, 0, 'YXZ');
+    o.quaternion.setFromEuler(euler);
     const components = o.getComponents();
     const componentIndex = components.findIndex(c => c.type === 'npc');
     npc.addNpc(o, componentIndex);
