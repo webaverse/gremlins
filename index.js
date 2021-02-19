453eb8e677b854cf95e8a8ae02fbec70eb324e8b
Avaer Kazmer <a@modules.io>
2021-02-19 12:48:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ import {renderer, scene, app, runtime, npc} from 'app';
 (async () => {
   const maxMobs = 20;
   for (let i = 0; i < maxMobs; i++) {
-    const o = await runtime.loadFile('./ghost/manifest.json');
+    const u = './ghost/manifest.json';
+    const o = await runtime.loadFile({
+      name: u,
+      url: u,
+    }, {
+      contentId: u,
+    });
     o.position.set(
       -10 + Math.random() * 20,
       -10 + Math.random() * 20,
