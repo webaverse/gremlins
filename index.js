cc75c4a538c7fdd94b279345959a82ad608c5419
Avaer Kazmer <a@modules.io>
2022-04-01 07:35:17 -0400

    Rewrite to use mob manager

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
 import * as THREE from 'three';
-import {renderer, scene, app, runtime, npc} from 'app';
+import metaversefile from 'metaversefile';
+const {useApp, createApp, useScene} = metaversefile;
 
-(async () => {
-  const maxMobs = 20;
-  for (let i = 0; i < maxMobs; i++) {
-    const u = 'https://webaverse.github.io/ghost/manifest.json';
-    const o = await runtime.loadFile({
-      name: u,
-      url: u,
-    }, {
-      contentId: u,
-    });
-    o.position.set(
-      -80, 0, 20
-    ).add(new THREE.Vector3(
-      -10 + Math.random() * 20,
-      -10 + Math.random() * 20,
-      -10 + Math.random() * 20
-    ));
-    const d = new THREE.Vector3(Math.random(), Math.random(), Math.random()).multiplyScalar(Math.PI*2);
-    const euler = new THREE.Euler(0, d.y, 0, 'YXZ');
-    o.quaternion.setFromEuler(euler);
-    const components = o.getComponents();
-    const componentIndex = components.findIndex(c => c.type === 'npc');
-    npc.addNpc(o, componentIndex);
-  }
+export default () => {
+  const app = useApp();
+  const scene = useScene();
 
-  app.addEventListener('frame', e => {
+  const range = app.getComponent('range') ?? 3;
+  const maxMobs = 10;
+  let ghostApps = null;
+  
+  (async () => {
+    const u = 'https://webaverse.github.io/ghost/';
+    const m = await metaversefile.import(u);
+    const r = () => -range + Math.random() * 2 * range;
+    
+    const promises = [];
+    for (let i = 0; i < maxMobs; i++) {
+      promises.push((async () => {
+        const ghostApp = await createApp();
+
+        ghostApp.position.copy(app.position)
+          .add(new THREE.Vector3(
+            r(),
+            0, // r(),
+            r(),
+          ));
+        ghostApp.quaternion.copy(app.quaternion);
+        scene.add(ghostApp);
+        ghostApp.updateMatrixWorld();
+
+        await ghostApp.addModule(m);
+
+        // console.log('new ghost app', ghostApp);
+      })());
+    }
+    ghostApps = await Promise.all(promises);
+  })();
+
+  /* useFrame(() => {
+    if (ghostApps) {
+      for (const ghostApp of ghostApps) {
+        ghostApp.update();
+      }
+    }
+  }); */
+
+  /* app.addEventListener('frame', e => {
     // npc.
-  });
-})();
+  }); */
+
+  return app;
+};
